8fbf433641c76fa79aab6469daac33fc8338754d
coderner <shreyashukla680@gmail.com>
2021-10-10 22:09:00 +0530

    fixed bug(resources values are getting added to left)

--- game.js
+++ game.js
@@ -17,7 +17,7 @@ let enemy__interval = 400;
 gameOver = false;
 const beams = []; //to hold all the projectile beams for all defender objects
 let score = 0;
-let win_score = 10;
+const win_score = 10;
 
 const mouse = {
   x: 10,
@@ -83,7 +83,7 @@ function ifcollide(first, second) {
 
 //beams
 class Beams {
-  constructor(x, y) {
+  constructor(x,y){
     this.x = x;
     this.y = y;
     this.width = 10;
@@ -91,35 +91,38 @@ class Beams {
     this.power = 20;
     this.speed = 5;
   }
-  move() {
+  move(){
     this.x += this.speed;
   }
-  create() {
+  create(){
     cntx.fillStyle = "black";
     cntx.beginPath();
     cntx.arc(this.x, this.y, this.width, 0, Math.PI * 2);
     cntx.fill();
   }
 }
-function handleBeams() {
-  for (let i = 0; i < beams.length; i++) {
+function handleBeams(){
+  for( let i = 0; i < beams.length; i++ ){
     beams[i].move();
     beams[i].create();
 
-    for (let j = 0; j < enemies.length; j++) {
-      if (enemies[j] && beams[i] && ifcollide(beams[i], enemies[j])) {
+    for(let j = 0; j < enemies.length; j++){
+      if (enemies[j] && beams[i] && ifcollide(beams[i],enemies[j]))
+      {
         enemies[j].health -= beams[i].power;
-        beams.splice(i, 1);
+        beams.splice(i,1);
         i--;
+
       }
     }
 
-    if (beams[i] && beams[i].x > gameboard.width - cellSize) {
+    if(beams[i] && beams[i].x > gameboard.width - cellSize){
       beams.splice(i, 1);
       i--; //no beam gets skipped
     }
   }
 }
+
 
 //defenders
 let numberOfResources = 300;
@@ -141,13 +144,14 @@ class defender {
     cntx.font = "30px Poppins";
     cntx.fillText(Math.floor(this.health), this.x + 15, this.y + 30);
   }
-  update_beam() {
-    if (this.shooting) {
-      this.timer++;
-      if (this.timer % 100 === 0) {
-        beams.push(new Beams(this.x + 50, this.y + 50));
+  update_beam(){
+    if(this.shooting){
+      this.timer ++;
+      if(this.timer % 100 === 0){
+        beams.push(new Beams(this.x + 50 ,this.y + 50));
       }
-    } else {
+    }
+    else{
       this.timer = 0;
     }
   }
@@ -173,11 +177,12 @@ function handleDefenders() {
     defenders[i].draw();
     defenders[i].update_beam();
 
-    if (enemyPosition.indexOf(defenders[i].y) !== -1) {
-      // beam wont be fired untill enemy occurs
-      defenders[i].shooting = true;
-    } else {
-      defenders[i].shooting = false;
+    if(enemyPosition.indexOf(defenders[i].y) !== -1) // beam wont be fired untill enemy occurs
+        {
+          defenders[i].shooting = true;
+        }    
+    else{
+          defenders[i].shooting = false;
     }
 
     for (let j = 0; j < enemies.length; j++) {
@@ -194,7 +199,8 @@ function handleDefenders() {
   }
 }
 
-//zombies
+
+//Enemies
 class Enemy {
   constructor(verticalPosition) {
     this.x = gameboard.width;
@@ -221,25 +227,24 @@ function enemy() {
   for (i = 0; i < enemies.length; i++) {
     enemies[i].change();
     enemies[i].draw();
-
+    
     if (enemies[i].x < 0) {
       gameOver = true;
     }
 
-    if (enemies[i].health <= 0) {
-      let earnedResources = enemies[i].maxHealth / 10;
+    if (enemies[i].health <= 0){
+      let earnedResources = enemies[i].maxHealth/10;
       numberOfResources += earnedResources;
-      score += earnedResources;
+      score += earnedResources; 
       const index_position = enemyPosition.indexOf(enemies[i].y);
-      enemyPosition.splice(index_position, 1);
-      enemies.splice(i, 1);
+      enemyPosition.splice(index_position,1);
+      enemies.splice(i,1);
       i--;
-    }
   }
-
-  if (gridcount % enemy__interval === 0 && score < win_score) {
-    let verticalPosition =
-      Math.floor(Math.random() * 6 + 1) * cellSize + cellGapping;
+ }
+  
+ if (gridcount % enemy__interval === 0 && score < win_score) {
+    let verticalPosition = Math.floor(Math.random() * 6 + 1) * cellSize + cellGapping;
     enemies.push(new Enemy(verticalPosition));
     enemyPosition.push(verticalPosition);
     if (enemy__interval > 180) enemy__interval -= 100;
@@ -249,34 +254,36 @@ function enemy() {
 //additional resources
 const value = [20, 30, 40];
 class Resource {
-  constructor() {
+  constructor(){
     this.x = Math.random() * (gameboard.width - cellSize);
     this.y = (Math.floor(Math.random() * 5) + 1) * cellSize + 25;
     this.width = cellSize * 0.6;
     this.height = cellSize * 0.6;
     this.value = value[Math.floor(Math.random() * value.length)];
   }
-  draw() {
+  draw(){
     cntx.fillStyle = "yellow";
     cntx.fillRect(this.x, this.y, this.width, this.height);
     cntx.fillStyle = "black";
-    cntx.font = "20px Orbiton";
+    cntx.font = "20px Orbitron";
     cntx.fillText(this.value, this.x + 15, this.y + 25);
   }
 }
-function handleResources() {
-  if (gridcount % 500 === 0 && score < win_score) {
+function handleResources(){
+  if(gridcount % 500 === 0 && score < win_score){
     resources.push(new Resource());
   }
-  for (let i = 0; i < resources.length; i++) {
+  for (let i = 0; i < resources.length; i++){
     resources[i].draw();
-    if (resources[i] && mouse.x && mouse.y && ifcollide(resources[i], mouse)) {
-      numberOfResources += resources[i].amount;
-      resources.splice(i, 1);
+    if (resources[i] && mouse.x && mouse.y && ifcollide(resources[i], mouse))
+    {
+      numberOfResources += resources[i].value;
+      resources.splice(i,1);
       i--;
     }
   }
 }
+
 
 function gameStatus() {
   cntx.fillStyle = "black";
@@ -288,7 +295,7 @@ function gameStatus() {
     cntx.font = "90 px Poppins";
     cntx.fillText("Game Over", 150, 330);
   }
-  if (score >= win_score && enemies.length === 0) {
+  if (score > win_score && enemies.length === 0){
     cntx.fillStyle = "black";
     cntx.font = "60px Orbitron";
     cntx.fillText("LEVEL COMPLETE", 130, 300);
