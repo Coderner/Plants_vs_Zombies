8f6070e0b4eaf6cc531cfbdf2f9bad88b11179a7
Ishan Polusta <polustaishan2001@gmail.com>
2021-10-11 16:46:02 +0530

    resolving conflicts

--- game.js
+++ game.js
@@ -19,12 +19,12 @@ const beams = []; //to hold all the projectile beams for all defender objects
 let score = 0;
 const win_score = 10;
 
-const mouse = {
-  x: 10,
-  y: 10,
-  width: 0.1,
-  height: 0.1,
-};
+// const mouse = {
+//   x: 10,
+//   y: 10,
+//   width: 0.1,
+//   height: 0.1,
+// };
 
 let gameboardposition = gameboard.getBoundingClientRect();
 gameboard.addEventListener("mousemove", function (e) {
@@ -83,7 +83,7 @@ function ifcollide(first, second) {
 
 //beams
 class Beams {
-  constructor(x,y){
+  constructor(x, y) {
     this.x = x;
     this.y = y;
     this.width = 10;
@@ -91,38 +91,35 @@ class Beams {
     this.power = 20;
     this.speed = 5;
   }
-  move(){
+  move() {
     this.x += this.speed;
   }
-  create(){
+  create() {
     cntx.fillStyle = "black";
     cntx.beginPath();
     cntx.arc(this.x, this.y, this.width, 0, Math.PI * 2);
     cntx.fill();
   }
 }
-function handleBeams(){
-  for( let i = 0; i < beams.length; i++ ){
+function handleBeams() {
+  for (let i = 0; i < beams.length; i++) {
     beams[i].move();
     beams[i].create();
 
-    for(let j = 0; j < enemies.length; j++){
-      if (enemies[j] && beams[i] && ifcollide(beams[i],enemies[j]))
-      {
+    for (let j = 0; j < enemies.length; j++) {
+      if (enemies[j] && beams[i] && ifcollide(beams[i], enemies[j])) {
         enemies[j].health -= beams[i].power;
-        beams.splice(i,1);
+        beams.splice(i, 1);
         i--;
-
       }
     }
 
-    if(beams[i] && beams[i].x > gameboard.width - cellSize){
+    if (beams[i] && beams[i].x > gameboard.width - cellSize) {
       beams.splice(i, 1);
       i--; //no beam gets skipped
     }
   }
 }
-
 
 //defenders
 let numberOfResources = 300;
@@ -144,14 +141,13 @@ class defender {
     cntx.font = "30px Poppins";
     cntx.fillText(Math.floor(this.health), this.x + 15, this.y + 30);
   }
-  update_beam(){
-    if(this.shooting){
-      this.timer ++;
-      if(this.timer % 100 === 0){
-        beams.push(new Beams(this.x + 50 ,this.y + 50));
+  update_beam() {
+    if (this.shooting) {
+      this.timer++;
+      if (this.timer % 100 === 0) {
+        beams.push(new Beams(this.x + 50, this.y + 50));
       }
-    }
-    else{
+    } else {
       this.timer = 0;
     }
   }
@@ -162,12 +158,11 @@ function handleDefenders() {
     defenders[i].draw();
     defenders[i].update_beam();
 
-    if(enemyPosition.indexOf(defenders[i].y) !== -1) // beam wont be fired untill enemy occurs
-        {
-          defenders[i].shooting = true;
-        }    
-    else{
-          defenders[i].shooting = false;
+    if (enemyPosition.indexOf(defenders[i].y) !== -1) {
+      // beam wont be fired untill enemy occurs
+      defenders[i].shooting = true;
+    } else {
+      defenders[i].shooting = false;
     }
 
     for (let j = 0; j < enemies.length; j++) {
@@ -187,7 +182,7 @@ function handleDefenders() {
 //Messages
 const messages = [];
 class message {
-  constructor(value, x, y, size, color){
+  constructor(value, x, y, size, color) {
     this.value = value;
     this.x = x;
     this.y = y;
@@ -196,13 +191,12 @@ class message {
     this.color = color;
     this.opacity = 1;
   }
-  change_y(){
+  change_y() {
     this.y -= 0.3;
     this.lifeSpan += 1;
-    if (this.opacity > 0.001)
-       this.opacity -= 0.01;
+    if (this.opacity > 0.001) this.opacity -= 0.01;
   }
-  draw(){
+  draw() {
     cntx.globalAlpha = this.opacity; //sets current transparency value
     cntx.fillStyle = this.color;
     cntx.font = this.size + "px Orbitron";
@@ -210,17 +204,16 @@ class message {
     cntx.globalAlpha = 1;
   }
 }
-function handlemessages(){
-  for(let i=0; i<messages.length; i++){
+function handlemessages() {
+  for (let i = 0; i < messages.length; i++) {
     messages[i].change_y();
     messages[i].draw();
-    if(messages[i].lifeSpan >= 50){
-       messages.splice(i,1);
-       i--;
-    }     
+    if (messages[i].lifeSpan >= 50) {
+      messages.splice(i, 1);
+      i--;
+    }
   }
 }
-
 
 //Enemies
 class Enemy {
@@ -249,26 +242,35 @@ function enemy() {
   for (i = 0; i < enemies.length; i++) {
     enemies[i].change();
     enemies[i].draw();
-    
+
     if (enemies[i].x < 0) {
       gameOver = true;
     }
 
-    if (enemies[i].health <= 0){
-      let earnedResources = enemies[i].maxHealth/10;
-      messages.push(new message("+ " + earnedResources ,130 ,20 , 20, "gold"));
-      messages.push(new message("+ " + earnedResources ,enemies[i].x ,enemies[i].y , 20, "black"));
+    if (enemies[i].health <= 0) {
+      let earnedResources = enemies[i].maxHealth / 10;
+      messages.push(new message("+ " + earnedResources, 130, 20, 20, "gold"));
+      messages.push(
+        new message(
+          "+ " + earnedResources,
+          enemies[i].x,
+          enemies[i].y,
+          20,
+          "black"
+        )
+      );
       numberOfResources += earnedResources;
-      score += earnedResources; 
+      score += earnedResources;
       const index_position = enemyPosition.indexOf(enemies[i].y);
-      enemyPosition.splice(index_position,1);
-      enemies.splice(i,1);
+      enemyPosition.splice(index_position, 1);
+      enemies.splice(i, 1);
       i--;
+    }
   }
- }
-  
- if (gridcount % enemy__interval === 0 && score < win_score) {
-    let verticalPosition = Math.floor(Math.random() * 6 + 1) * cellSize + cellGapping;
+
+  if (gridcount % enemy__interval === 0 && score < win_score) {
+    let verticalPosition =
+      Math.floor(Math.random() * 6 + 1) * cellSize + cellGapping;
     enemies.push(new Enemy(verticalPosition));
     enemyPosition.push(verticalPosition);
     if (enemy__interval > 180) enemy__interval -= 100;
@@ -278,14 +280,14 @@ function enemy() {
 //additional resources
 const value = [20, 30, 40];
 class Resource {
-  constructor(){
+  constructor() {
     this.x = Math.random() * (gameboard.width - cellSize);
     this.y = (Math.floor(Math.random() * 5) + 1) * cellSize + 25;
     this.width = cellSize * 0.6;
     this.height = cellSize * 0.6;
     this.value = value[Math.floor(Math.random() * value.length)];
   }
-  draw(){
+  draw() {
     cntx.fillStyle = "yellow";
     cntx.fillRect(this.x, this.y, this.width, this.height);
     cntx.fillStyle = "black";
@@ -293,23 +295,31 @@ class Resource {
     cntx.fillText(this.value, this.x + 15, this.y + 25);
   }
 }
-function handleResources(){
-  if(gridcount % 500 === 0 && score < win_score){
+function handleResources() {
+  if (gridcount % 500 === 0 && score < win_score) {
     resources.push(new Resource());
   }
-  for (let i = 0; i < resources.length; i++){
+  for (let i = 0; i < resources.length; i++) {
     resources[i].draw();
-    if (resources[i] && mouse.x && mouse.y && ifcollide(resources[i], mouse))
-    {
+    if (resources[i] && mouse.x && mouse.y && ifcollide(resources[i], mouse)) {
       numberOfResources += resources[i].value;
-      messages.push(new message("+ " + resources[i].value, resources[i].x, resources[i].y, 20, "black"));
-      messages.push(new message("+ " + resources[i].value,  120, 55, 20, "gold"));
-      resources.splice(i,1);
+      messages.push(
+        new message(
+          "+ " + resources[i].value,
+          resources[i].x,
+          resources[i].y,
+          20,
+          "black"
+        )
+      );
+      messages.push(
+        new message("+ " + resources[i].value, 120, 55, 20, "gold")
+      );
+      resources.splice(i, 1);
       i--;
     }
   }
 }
-
 
 function gameStatus() {
   cntx.fillStyle = "black";
@@ -321,7 +331,7 @@ function gameStatus() {
     cntx.font = "90 px Poppins";
     cntx.fillText("Game Over", 150, 330);
   }
-  if (score > win_score && enemies.length === 0){
+  if (score > win_score && enemies.length === 0) {
     cntx.fillStyle = "black";
     cntx.font = "60px Orbitron";
     cntx.fillText("LEVEL COMPLETE", 130, 300);
@@ -343,9 +353,10 @@ gameboard.addEventListener("click", function () {
   if (numberOfResources >= defenderPower) {
     defenders.push(new defender(gridPositionX, gridPositionY));
     numberOfResources -= defenderPower;
-  }
-  else{
-    messages.push(new message("Need more Resources", mouse.x, mouse.y, 20, "blue"));
+  } else {
+    messages.push(
+      new message("Need more Resources", mouse.x, mouse.y, 20, "blue")
+    );
   }
 });
 
